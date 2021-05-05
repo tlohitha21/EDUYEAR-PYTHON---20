216993345376c9f67c059985edffc422432fe3f7
tlohitha21 <83698601+tlohitha21@users.noreply.github.com>
2021-05-05 14:38:48 +0530

    Add files via upload

---
+++ DAY-02.py
@@ -0,0 +1,21 @@
+name="Lohitha Reddy"
+age=25
+salary=40000.00
+print("My name is {},I am {} old and I am earning {}/- per month." .format(name,age,salary))
+#Type conversion functions from int to str,float
+print(type(age))
+print(type(str(age)))
+print(type(float(age)))
+#Type conversion functions from float to int,str
+print(type(salary))
+print(type(int(salary)))
+print(type(str(salary)))
+#Type conversion functions from str to int,float
+print(type(name))
+#print(type(str(name))) getting value error:invalid literal
+name1='2014'
+print(type(int(name1)))
+print(type(float(name1)))
+
+
+

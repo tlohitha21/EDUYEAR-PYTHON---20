7a82688555997bcaf25278289153877b60ab8b89
tlohitha21 <83698601+tlohitha21@users.noreply.github.com>
2021-05-08 12:09:34 +0530

    Add files via upload

---
+++ Day-04.py
@@ -0,0 +1,4 @@
+n=str(input("enter any string:"))
+print("count of occurences of 'y' in a given string:",n.count('y'))
+print("even indexes in a given string:",n[::2])
+print("swapcase of a given string:",n.swapcase())
